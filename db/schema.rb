f7e0f0f37490c741fc44b35395b935a116a4f91d
Kealoha1988 <kealohaabe1988@gmail.com>
2021-07-28 00:11:00 -0400

    created Messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_214556) do
+ActiveRecord::Schema.define(version: 2021_07_28_040913) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2021_06_03_214556) do
     t.string "skinTone"
     t.integer "likes", default: 0
     t.index ["user_id"], name: "index_characters_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
