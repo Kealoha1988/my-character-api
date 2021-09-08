b889e4ec39f1b1e57d14b22aae09c89052cbda94
Kealoha1988 <kealohaabe1988@gmail.com>
2021-09-08 15:22:50 -0400

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_28_040913) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "hairIndex"
